b57c0c7ef4a92d543bc9d12ede137fae0abf1c67
Alone <a.sinzig02@gmail.com>
2018-10-30 21:17:48 +0100

    added blobUser class to MBlib

--- MBlib.py
+++ MBlib.py
@@ -1,25 +1,50 @@
-#Anmelde Library
+# Anmelde Library
 import json
+#install: pip install --upgrade google-api-python-client
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
-def TokenReal(token):
-    #test if Token real
-    return True
+# Oeffnung der Json Files
+class jsonClass:
+    @classmethod
+    def open(cls, path):
+        jsonFile = open(path, 'r')
+        data = json.loads(jsonFile.read())
+        jsonFile.close()
+        return data
+    @classmethod
+    def save(cls, path, data):
+        dataJSON = json.dumps(data, indent=2)
+        jsonFile = open(path, 'w')
+        jsonFile.write(dataJSON)
+        jsonFile.close()
 
-def getUserId(token):
-    if TokenReal(token) == True:
+# Klasse des Nutzers
+class blobUser:
+    def __init__(self, token):
+        if not self.__tokenReal(token):
+            self.isOk = False
+            return
+        self.isOk = False
+        path = self.homeFolder + "/user/" + self.userId + ".json"
+        userFile = jsonClass.open(path)
+        self.name = userFile["info"]["name"]
+
+    def __tokenReal(self, token):
+        try:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), self.homeFiles["clientId"])
+            self.userId = idinfo['sub']
+            self.mail = idinfo['email']
+        except:
+            return False
+        return True
+
+    def write(self, text):
         pass
-        #Get User ID
-    return "error"
 
-def openJson(path):
-    jsonFile = open(path,'r')
-    data = json.loads(jsonFile.read())
-    jsonFile.close()
-    return data
-
-def saveJson(path, data):
-    dataJSON = json.dumps(data, indent=2)
-    jsonFile = open(path, 'w')
-    jsonFile.write(dataJSON)
-    jsonFile.close()
-
+    @classmethod
+    def setHomeFolder(cls):
+        path = 'files.json'
+        data = jsonClass.open(path)
+        cls.homeFiles = data
+        cls.homeFolder = data["HomeFolder"]
