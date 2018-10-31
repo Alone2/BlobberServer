7b5512c7bba21eb1c426924425d834be3c92c143
Alone2 <a.sinzig02@gmail.com>
2018-10-31 21:14:23 +0100

    added content sorting server

---
+++ server.py
@@ -0,0 +1,17 @@
+#In disem Programm wird der Content sortiert
+import time
+
+def newTrending():
+    pass
+    
+def newHot():
+    pass
+
+if __name__ == "__main__":
+    newTrending()
+    newHot()
+    time.sleep(3600)
+
+
+
+#Hier kommt sollte man den Content aufrufen
