86cc7525230066ab2c6c1f2487dea2f028b3df34
Alone2 <a.sinzig02@gmail.com>
2018-10-31 17:01:58 +0100

    added comments, improved code

--- MBlib.py
+++ MBlib.py
@@ -1,6 +1,9 @@
 # Anmelde Library
 import json
-#install: pip install --upgrade google-api-python-client
+import os
+import time
+# install: pip install --upgrade google-api-python-client
+#          pip install requests
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
@@ -22,29 +25,57 @@ class jsonClass:
 # Klasse des Nutzers
 class blobUser:
     def __init__(self, token):
+        # Testen ob der Account echt ist. Wenn ja -> isOk = True
         if not self.__tokenReal(token):
             self.isOk = False
             return
-        self.isOk = False
-        path = self.homeFolder + "/user/" + self.userId + ".json"
-        userFile = jsonClass.open(path)
-        self.name = userFile["info"]["name"]
+        self.isOk = True
+        
+        # Path wird definiert
+        self.path = self.homeFolder + "/user/" + self.userId + ".json"
+        
+        # Werte vom json-file werden an blobUser übertragen
+        userFile = jsonClass.open(self.path)
+        self.firstName = userFile["info"]["firstName"]
+        self.lastName = userFile["info"]["lastName"]
+        self.mail = userFile["info"]["mail"]
 
     def __tokenReal(self, token):
+        # Wird bei Google getestet ob der Acc. echt ist
         try:
-            idinfo = id_token.verify_oauth2_token(token, requests.Request(), self.homeFiles["clientId"])
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), self.homeData["clientId"])
             self.userId = idinfo['sub']
-            self.mail = idinfo['email']
+            
+            # Path wird definiert
+            self.path = self.homeFolder + "/user/" + self.userId + ".json"
         except:
             return False
+        
+        # Wenn der User noch nicht im System ist, wird ihm ein File erstellt
+        # -> Wenn der User sich manuell anmelden sollte -> Später durch Instanz ersetzen
+        if not os.path.isfile(self.path):
+            jsonData = {}
+            storedInfo = {"mail":idinfo["email"],"firstName":idinfo["given_name"],"lastName":idinfo["family_name"]}
+            jsonData['info'] = storedInfo
+            jsonClass.save(self.path, jsonData)
+
         return True
 
     def write(self, text):
-        pass
+        # Zeit angeben
+        zeit = time.localtime()
+        zeitVar = list(zeit[0:7]) 
+
+        #Text wird geschpeichert
+        userFile = jsonClass.open(self.path)
+        userFile["text"] = {"time":zeitVar,"text":text}
+        jsonClass.save(self.path, userFile)
 
     @classmethod
     def setHomeFolder(cls):
+        #Der Ort der Dateien wird von files.json ausgelesen
         path = 'files.json'
         data = jsonClass.open(path)
-        cls.homeFiles = data
+        # Und als homeData gespeichert
+        cls.homeData = data
         cls.homeFolder = data["HomeFolder"]
