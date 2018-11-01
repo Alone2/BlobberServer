38648b457f53f0a0af61b1db8ae7c84e0c75310b
Alone <a.sinzig02@gmail.com>
2018-11-01 22:10:05 +0100

    changed structure for later

--- MBlib.py
+++ MBlib.py
@@ -8,31 +8,42 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 
 # Oeffnung der Json Files
-class jsonClass:
-    @classmethod
-    def open(cls, path):
+class dataClass:
+    @staticmethod
+    def open(path):
         jsonFile = open(path, 'r')
         data = json.loads(jsonFile.read())
         jsonFile.close()
         return data
-    @classmethod
-    def save(cls, path, data):
+
+    @staticmethod
+    def save(path, data):
         dataJSON = json.dumps(data, indent=2)
         jsonFile = open(path, 'w')
         jsonFile.write(dataJSON)
         jsonFile.close()
 
+    @classmethod
+    def getHomeData(cls):
+        #Der Ort der Dateien wird von files.json ausgelesen und ausgegeben
+        path = 'files.json'
+        data = cls.open(path)
+        return data
+
 # Klasse des Nutzers
 class blobUser:
     def __init__(self, token):
+        #Home angeben
+        self.homeData = dataClass.getHomeData()
+        self.homeFolder = dataClass.getHomeData()["HomeFolder"]
         # Testen ob der Account echt ist. Wenn ja -> isOk = True
         if not self.__tokenReal(token):
             self.isOk = False
             return
         self.isOk = True
-                
+        
         # Werte vom json-file werden an blobUser übertragen
-        userFile = jsonClass.open(self.path)
+        userFile = dataClass.open(self.path)
         self.firstName = userFile["info"]["firstName"]
         self.lastName = userFile["info"]["lastName"]
         self.mail = userFile["info"]["mail"]
@@ -59,7 +70,7 @@ class blobUser:
             storedInfo = {"mail":idinfo["email"],"firstName":idinfo["given_name"],"lastName":idinfo["family_name"]}
             jsonData['info'] = storedInfo
             jsonData['text'] = []
-            jsonClass.save(self.path, jsonData)
+            dataClass.save(self.path, jsonData)
 
         return True
 
@@ -68,16 +79,9 @@ class blobUser:
         zeit = time.localtime()
         zeitVar = list(zeit[0:7]) 
 
-        #Text wird geschpeichert
-        userFile = jsonClass.open(self.path)
+        # Text wird geschpeichert
+        userFile = dataClass.open(self.path)
+        # upvotes, id, id von comment soll gesprichert werden (Id zufällig gewählt->Test ob
+        # es sie schon gibt -> index)
         userFile["text"].append({"time":zeitVar,"text":text})
-        jsonClass.save(self.path, userFile)
-
-    @classmethod
-    def setHomeFolder(cls):
-        #Der Ort der Dateien wird von files.json ausgelesen
-        path = 'files.json'
-        data = jsonClass.open(path)
-        # Und als homeData gespeichert
-        cls.homeData = data
-        cls.homeFolder = data["HomeFolder"]
+        dataClass.save(self.path, userFile)
