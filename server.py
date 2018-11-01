4dbb1be4e0e4c427a7d55509279d6c4d7f009e20
Alone <a.sinzig02@gmail.com>
2018-11-01 22:38:46 +0100

    added comments

--- server.py
+++ server.py
@@ -1,17 +1,39 @@
 #In disem Programm wird der Content sortiert
 import time
+from MBlib import dataClass
 
 def newTrending():
+    # Wie newHot()
     pass
     
 def newHot():
-    pass
+    #Liste mit Blobs wird geoffnet
+    home = dataClass.getHomeData()["home"]
+    indexPath = home + "/index.json"
+    index = dataClass.open(indexPath)
+
+    hot = []
+    # Für jeden Blob wird der Text im For-Loop ausgeführt
+    for i in index:
+        # upvotes (downvotes abgezogen)
+        upvotes = i["upvotes"]
+        # commentsNumber ist die Anzahl Kommentare
+        commentsNumber = i["commentsNumber"]
+        # Wann der Blob veröffentlicht wurde
+        datum = i["date"]
+        
+        # Hier kommt ein Algorithmus
+        
+        # Blobs die es in Hot eingeordnet werden sollen, werden in die Liste hot gespeichert
+        hot.append(i["id"])
+
+    # Hot wird als Datei gespeichert
+    hotPath = home + "/sorting/hot.json"
+    dataClass.save(hotPath, hot)
+  
 
 if __name__ == "__main__":
-    newTrending()
-    newHot()
-    time.sleep(3600)
-
-
-
-#Hier kommt sollte man den Content aufrufen
+    while True:
+        newTrending()
+        newHot()
+        time.sleep(3600)
