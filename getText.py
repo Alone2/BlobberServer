c056d1960707ac967376fb818431adca27e31976
Alone2 <a.sinzig02@gmail.com>
2018-11-04 17:20:34 +0100

    getText.py works now

--- getText.py
+++ getText.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 print("Content-type: text/html\n\n")
 import cgi
+import json
+from MBlib import sortingClass
 
 arguments = cgi.FieldStorage()
 
-if arguments["sorting"] == "new":
-    pass
-elif arguments["sorting"] == "trending":
-    pass
+blobs = sortingClass.getBlobJsonList(arguments["sorting"].value, int(arguments["von"].value), int(arguments["bis"].value))
+#print(json.dumps(blobs, indent=2))
+print(blobs)
