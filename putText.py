cc16f9d8df3c79a1af7e01fa6ff92292d3c885cf
Alone2 <a.sinzig02@gmail.com>
2018-11-10 23:55:26 +0100

    added comments to putText

--- putText.py
+++ putText.py
@@ -8,7 +8,7 @@ from MBlib import dataClass
 def main():
     arguments = cgi.FieldStorage()
     # Wenn die Argumente nicht da sind, wird eine Error-Message ausgegeben
-    if (not "idTkn" in arguments) or (not "text" in arguments):
+    if (not "idTkn" in arguments) or (not "text" in arguments) or (not "comment" in arguments):
         print("error - arguments missing")
         return
     # Wenn der Blob mehr als 300 Zeichen hat, wird eine Error-Message ausgegeben
@@ -22,9 +22,17 @@ def main():
     if user.isOk == False:
         print("error - nicht angemeldet")
         return
-    # Blob wird gespeichert
-    user.write(text)
-    print("done")
+    # Wird geschaut ob es en Kommentar ist
+    if arguments["comment"].value == "true":
+        if not "postId" in arguments:
+            print("error - arguments missing")
+            return
+        user.comment(text, arguments["postId"].value)
+        return
+    else:
+        # Blob wird gespeichert
+        user.write(text)
+        print("done")
 
 if __name__ == "__main__":
     # Beim Starten wird die Funktion main ausgeführt
